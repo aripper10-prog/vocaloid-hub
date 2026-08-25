091750d789a2ddd58ab47b7fd13b7fa25e65587e
aripper10-prog <aripper10@gmail.com>
2026-08-26 00:45:57 +0900

    constants.ts の削除

--- lib/constants.ts
+++
@@ -1,8 +0,0 @@
-// 外部APIのエンドポイントを安全に一元管理するファイル
-export const API_ENDPOINTS = {
-  VOCADB_SONGS: 'https://vocadb.net/api/songs',
-  VOCADB_ARTISTS: 'https://vocadb.net/api/artists',
-  GEMINI_FLASH: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
-  YOUTUBE_SEARCH: 'https://www.googleapis.com/youtube/v3/search',
-  YOUTUBE_VIDEOS: 'https://www.googleapis.com/youtube/v3/videos',
-};
